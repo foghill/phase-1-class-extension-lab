3d166516905a7e5fcaf669415cc2c4763bdf8e56
foghill <ben@thefoghill.com>
2022-04-06 13:38:44 -0400

    commit tree 1f324370c462b2763fad11cad504aa416f3d71d5

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+
+class Polygon {
+
+    constructor(sides) {
+        this.sides = sides
+    }
+
+    get countSides() {
+        return this.sides.length
+    }
+
+    get perimeter() {
+
+    }
+
+}
+
+class Triangle extends Polygon {
+
+get isValid() {
+
+}
+
+}
+
+
+class Square extends Polygon {
+
+    get isValid() {
+    
+    }
+
+    get area() {
+    
+    }
+
+    
+}
